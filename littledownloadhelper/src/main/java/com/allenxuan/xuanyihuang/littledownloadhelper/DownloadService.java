5f38b6586c734f33fc68cf5131a9c7a712b2ead4
xuanyihuang <allenxuan@gmail.com>
2017-03-14 13:02:10 +0800

    LittleDownloadHelper v0.1.0 is complete.

--- littledownloadhelper/src/main/java/com/allenxuan/xuanyihuang/littledownloadhelper/DownloadService.java
+++ littledownloadhelper/src/main/java/com/allenxuan/xuanyihuang/littledownloadhelper/DownloadService.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2017 Xuanyi Huang.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.allenxuan.xuanyihuang.littledownloadhelper;
 
-import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -12,8 +27,11 @@ import android.os.Environment;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
-
 import java.io.File;
+
+/**
+ * Created by xuanyihuang on 08/03/2017.
+ */
 
 public class DownloadService extends Service {
     private DownloadTask downloadTask;
